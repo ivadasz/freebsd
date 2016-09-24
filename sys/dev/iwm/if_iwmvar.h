396a72f17256534e8cf61527ee1219fc0cbbd9b9
Imre Vadász <ivadasz@FreeBSD.org>
2016-09-24 18:39:21 +0200

    [iwm] Sync valid_tx_ant and valid_rx_ant mask handling with iwlwifi.
    * This fixes the phy_cfg field sent in the iwm_send_phy_cfg_cmd()
      command, which wasn't taking into account the valid_rx_ant and
      valid_tx_ant masks from nvm_data before.

--- sys/dev/iwm/if_iwmvar.h
+++ sys/dev/iwm/if_iwmvar.h
@@ -172,6 +172,10 @@ struct iwm_fw_info {
 		} fw_sect[IWM_UCODE_SECT_MAX];
 		int fw_count;
 	} fw_sects[IWM_UCODE_TYPE_MAX];
+
+	uint32_t phy_config;
+	uint8_t valid_tx_ant;
+	uint8_t valid_rx_ant;
 };
 
 struct iwm_nvm_data {
@@ -450,7 +454,6 @@ struct iwm_softc {
 	const char		*sc_fwname;
 	bus_size_t		sc_fwdmasegsz;
 	struct iwm_fw_info	sc_fw;
-	int			sc_fw_phy_config;
 	struct iwm_tlv_calib_ctrl sc_default_calib[IWM_UCODE_TYPE_MAX];
 
 	struct iwm_nvm_data	*nvm_data;
